c06d7831049f28f8b81bd07112eb6520b06b66b6
Passeriform <utbh23041999@gmail.com>
2022-05-30 08:30:04 +0530

    Fix XOB launch script.

--- dot_config/xob/executable_launch.sh
+++ dot_config/xob/executable_launch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python "./scripts/volume-monitor.py" | xob -c "${HOME}/.cache/wal/xob-style.cfg" -s volume
+python "$(dirname ${BASH_SOURCE[0]})/scripts/volume-monitor.py" | xob -c "${HOME}/.cache/wal/xob-style.cfg" -s volume
